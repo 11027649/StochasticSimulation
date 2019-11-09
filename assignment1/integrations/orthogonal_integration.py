8bf0f65f48572ab136afe84139c3dd1569adc866
11027649 <natasja.wezel@hotmail.com>
2019-11-09 14:56:16 +0000

    Implement orthogonal integration

--- assignment1/integrations/orthogonal_integration.py
+++ assignment1/integrations/orthogonal_integration.py
@@ -16,87 +16,55 @@ def main():
     real_min, real_max = -1, 2
     im_min, im_max = -1.25, 1.25
 
-    # amount of points for integration (it must be able to be squared)
-    N = 9
+    # amount of points for integration
+    total_points = np.arange(1000, 1000000, 900)
 
-    # sanity check
-    assert math.sqrt(N) == math.floor(math.sqrt(N)), "must be able to be squared"
+    for samples in total_points:
+        print("points: ", samples)
 
-    # define N intervals in both real and imaginary direction
-    real_intervals = np.linspace(real_min, real_max, N + 1)
-    im_intervals = np.linspace(im_min, im_max, N + 1)
+        # sanity check
+        assert math.sqrt(samples) == math.floor(math.sqrt(samples)), "must be able to be squared"
 
-    total_area = (abs(real_min) + abs(real_max)) * (abs(im_min) + abs(im_max))
+        # define N intervals in both real and imaginary direction
+        real_intervals = np.linspace(real_min, real_max, N + 1)
+        im_intervals = np.linspace(im_min, im_max, N + 1)
 
-    generate_o(real_min, real_max, im_min, im_max, N)
-    # area = orthogonal_integration(real_intervals, im_intervals, max_iters, total_area, N)
+        total_area = (abs(real_min) + abs(real_max)) * (abs(im_min) + abs(im_max))
 
-    # with open("../results/orthogonal_integration_results.csv", 'a') as resultsfile:
-    #     writer = csv.writer(resultsfile, delimiter=',')
-    #     writer.writerow([max_iters, N, area])
+        for test in range(5000):
+            starttime = time.time()
+
+            real_parts, im_parts = generate_o(real_min, real_max, im_min, im_max, samples)
+            area = orthogonal_integration(real_parts, im_parts, samples, max_iters, total_area)
+
+            endtime = time.time()
+
+            with open("../results/orthogonal_integration_results.csv", 'a') as resultsfile:
+                writer = csv.writer(resultsfile, delimiter=',')
+                writer.writerow([max_iters, N, area, endtime-starttime])
 
 
-@jit(nopython=True)
-def orthogonal_integration(real_intervals, im_intervals, maxiter, total_area, N):
-    """ Calculates the area of the Mandelbrot Set using 2D latin cube integration.
-        Does this using N points. Latin cube sampling spreads the sample points
-        'more uniformly' across all of the possible values than the basic MC method.
+def orthogonal_integration(real_parts, im_parts, samples, maxiter, total_area):
+    """ Calculates the area of the Mandelbrot Set using 2D orthogonal sampling.
+        Does this using N points. Orthogonal sampling spreads the sample points
+        'more uniformly' across all of the possible values than the basic MC/LH method.
         In theory we found that the area has to be somewhere around
         sqrt(6*pi - 1) - e = 1.506591... (see report for details) """
 
-    total_points = 9
-    subgrids_per_axis = 3
-    subgrids = subgrids_per_axis * subgrids_per_axis
-    subpoints = total_points /  subgrids_per_axis
-
     points_in_set = 0
 
-    # draw a list of tuples of coordinates, then shuffle
-    points = np.empty(shape=(N, 2))
-
-    subgrid_nr = 0
-
-    start_real = subgrid_nr * subpoints
-    end_real = start_real + subpoints + 1
-
-    for i in range(subgrids):
-        print("Subgrid nummer: ", subgrid_nr)
-
-        start_im = subgrid_nr %  subgrids_per_axis * subpoints # 2 klopt niet als het om meer subgrids gaat
-        end_im = start_im + subpoints + 1 # also include the last point/boundary
-
-        im_subgrid_range = im_intervals[start_im:end_im]
-        real_subgrid_range = real_intervals[start_real:end_real]
-
-        print("real (x): ", real_subgrid_range, len(real_subgrid_range))
-        print("im (y): ", im_subgrid_range, len(im_subgrid_range))
-
-        for i in range(subpoints):
-            points[i, 0] = np.random.uniform(real_intervals[i], real_intervals[i + 1])
-            points[i, 1] = np.random.uniform(im_intervals[i], im_intervals[i + 1])
-
-        # shuffle y (im) coordinates
-        np.random.shuffle(points[:, 1])
-
-        # move to next subgrid
-        subgrid_nr += 1
-
-        if subgrid_nr %  subgrids_per_axis == 0:
-            start_real = start_real + subpoints
-            end_real = start_real + subpoints + 1
-
-
     # loop over each point in the plane and count the number of points in the set
-    for n in range(N):
-        c = complex(points[n, 0], points[n , 1])
+    for n in range(samples):
+        c = complex(real_parts[n], im_parts[n])
 
         if in_mandelbrot(c, maxiter):
             points_in_set += 1
 
-    return float(points_in_set) * total_area / total_points
+    return float(points_in_set) * total_area / samples
 
 
 def generate_o(real_min, real_max, im_min, im_max, samples):
+    """ Generates the coordinates of the points using orthogonal sampling. """
 
     values_i = []
     values_r = []
@@ -109,9 +77,6 @@ def generate_o(real_min, real_max, im_min, im_max, samples):
     xlist = [[0 for i in range(subgrids)] for j in range(subgrids)]
     ylist = [[0 for i in range(subgrids)] for j in range(subgrids)]
 
-    for row in xlist:
-        print(row)
-
     m = 0
 
     for i in range(subgrids):
@@ -119,37 +84,13 @@ def generate_o(real_min, real_max, im_min, im_max, samples):
             xlist[i][j] = ylist[i][j] = m
             m += 1
 
-
     np.random.shuffle(xlist)
     np.random.shuffle(ylist)
-
-
-    for row in xlist:
-        print(row)
 
     for i in range(subgrids):
         for j in range(subgrids):
             values_i.append(im_min + scale_im * (xlist[i][j] + np.random.random() ))
             values_r.append(real_min + scale_real * (ylist[j][i] + np.random.random() ))
-
-    for i in range(len(values_i)):
-        print(values_i[i], values_r[i])
-
-    plt.xlim((real_min, real_max))
-    plt.ylim((im_min, im_max))
-    plt.ylabel("Imaginary")
-    plt.xlabel("Real")
-
-    plt.scatter(values_r, values_i)
-
-    for i in range(9):
-
-        plt.hlines(im_min + i * scale_im, real_min, real_max)
-        plt.vlines(real_min + i * scale_real, im_min, im_max)
-
-    plt.savefig("test")
-
-
 
     return values_i, values_r
 
